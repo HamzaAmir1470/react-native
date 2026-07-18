0e5aeeed3b45384569d7f79348471e37bb9b1f87
HamzaAmir1470 <hamzaamir9733@gmail.com>
2026-07-18 10:19:19 +0500

    completed lesson 10

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,6 +1,7 @@
 import { Tabs } from "expo-router"
 import { useColorScheme } from "react-native"
 import { Colors } from "../../constants/Colors"
+import { Ionicons } from "@expo/vector-icons"
 
 export default function DashboardLayout() {
     const colorScheme = useColorScheme()
@@ -17,15 +18,15 @@ export default function DashboardLayout() {
         >
             <Tabs.Screen
                 name="profile"
-                options={{ title: "Profile" }}
+                options={{ title: "Profile", tabBarIcon: ({ focused }) => <Ionicons name={focused ? "person" : "person-outline"} size={24} color={focused ? theme.iconColorFocused : theme.iconColor} /> }}
             />
             <Tabs.Screen
                 name="books"
-                options={{ title: "Books" }}
+                options={{ title: "Books", tabBarIcon: ({ focused }) => <Ionicons name={focused ? "book" : "book-outline"} size={24} color={focused ? theme.iconColorFocused : theme.iconColor} /> }}
             />
             <Tabs.Screen
                 name="create"
-                options={{ title: "Create", }}
+                options={{ title: "Create", tabBarIcon: ({ focused }) => <Ionicons name={focused ? "add-circle" : "add-circle-outline"} size={24} color={focused ? theme.iconColorFocused : theme.iconColor} /> }}
             />
         </Tabs>
     )
