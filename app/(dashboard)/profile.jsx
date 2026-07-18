d41c5a298616e1a8de04dbeee0e444e007a7d791
HamzaAmir1470 <hamzaamir9733@gmail.com>
2026-07-18 14:34:14 +0500

    Completed Lesson 18

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -1,10 +1,15 @@
 import { StyleSheet } from 'react-native'
+import { useUser } from "../../hooks/useUser"
 
 import Spacer from "../../components/Spacer"
 import ThemedText from "../../components/ThemedText"
 import ThemedView from "../../components/ThemedView"
+import ThemedButton from '../../components/ThemedButton'
 
 const Profile = () => {
+
+    const { logout } = useUser()
+
     return (
         <ThemedView style={styles.container}>
 
@@ -15,7 +20,9 @@ const Profile = () => {
 
             <ThemedText>Time to start reading some books...</ThemedText>
             <Spacer />
-
+            <ThemedButton onPress={logout}>
+                <Text style={{ color: 'white', fontWeight: 'bold' }}>Logout</Text>
+            </ThemedButton>
         </ThemedView>
     )
 }
